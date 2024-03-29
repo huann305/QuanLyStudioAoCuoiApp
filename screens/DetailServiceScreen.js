136371724f5a2b0fb2cb5ecc176e72f6b44d3ed1
Hoang Yen <yenphph34781@fpoly.edu.vn>
2024-03-29 07:52:57 +0700

    HoangYen

--- screens/DetailServiceScreen.js
+++ screens/DetailServiceScreen.js
@@ -1,63 +1,41 @@
-// import React, { useCallback, useState, useEffect } from 'react';
-// import { View, Text, Image } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, Image } from 'react-native';
 
-// import BASE_URL from '../base/BASE_URL';
-// import { Checkbox } from 'react-native-paper';
-// const URL_detailService = `${BASE_URL}/servicedetails`;
+import BASE_URL from '../base/BASE_URL';
+const URL_services = `${BASE_URL}/services`;
+const URL_servicedetails = `${BASE_URL}/serviceDetails`;
 
-// const DetailServiceScreen = ({ route }) => {
-//     const [detailService, setdetailService] = useState([]);
-//     const idItem = route.params.idItem;
-//     const [newDetail, setnewDetail] = useState();
-//     const [statusU, setstatusU] = useState('');
-//     const [checked, setchecked] = useState(false);
+const DetailServiceScreen = ({ route }) => {
+ const [obj, setobj] = useState({serviceName: '', description: '', image: ''});
+ const [objD, setobjD] = useState({title: '', price: ''});
 
-//     // useEffect(() => {
-//     //     const newDetail = detailService.find(x => x.idService._id === idItem);
-//     //     setnewDetail(newDetail);
-//     // }, [newDetail, detailService, idItem]);
+  useEffect(() => {
+    fetch(URL_servicedetails + `/${route.params.idItemDetail}`)
+      .then(response => response.json())
+      .then(json => setobjD(json))
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
 
-//     const detailServiceData = useCallback(() => {
-//         fetch(URL_detailService + `?idService=${idItem}`)
-//             .then(response => response.json())
-//             .then(json => setdetailService(json))
-//             .catch(error => {
-//                 console.error(error);
-//             });
-//     }, []);
+  useEffect(() => {
+    fetch(URL_services + `/${route.params.idItemm}`)
+      .then(response => response.json())
+      .then(json => setobj(json))
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
 
-//     useEffect(() => {
-//         detailServiceData();
-//     }, [detailServiceData]);
-//     //hiện thị đã tích , chưa tích
-//     // useEffect(() => {
-//     //     setstatusU(detailService.status) === 'Tạm dừng';
-//     //     console.log(idItem.status);
-//     // }, [statusU, idItem, detailService]);
-//     return (
-//         <View>
-//             {detailService.idService.image && <Image style={{ width: 100, height: 100 }} source={{ uri: detailService.idService.image }} />}
-//             <Text>{detailService.idService.serviceName}</Text>
-//             <Text>{detailService.idService.description}</Text>
-//             <Text>{newDetail.price}</Text>
-//             <Text>{newDetail.title}</Text>
-//             {/* <Checkbox
-//                 status={checked === statusU ? 'checked' : 'unchecked'}
-//                 onPress={() => setchecked(!checked)}
-//             /> */}
-
-//         </View>
-//     );
-// }
-
-// export default DetailServiceScreen;
-import React from 'react';
-import { View, Text } from 'react-native';
-
-const DetailServiceScreen = () => {
   return (
     <View>
-      <Text></Text>
+      <Image source={{ uri: obj.image }} />
+      <Text>{obj.serviceName}</Text>
+      {/* khong hiện text dịch vụ */}
+      <Text>{obj.description} + kjjkjjhk</Text>
+      <Text>{objD.title}+jnknk</Text>
+      <Text>{objD.price}+jnknk</Text>
+      <Text>{objD.status}+jnknk</Text>
     </View>
   );
 }
--- screens/ServiceManagementScreen.js
+++ screens/ServiceManagementScreen.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { View, Text, SafeAreaView, FlatList, TouchableOpacity, Image, StyleSheet, Modal, TextInput, Pressable, Alert } from 'react-native';
-
+import BASE_URL from '../base/BASE_URL';
 const URL_services = `${BASE_URL}/services`;
-const URL_servicedetails = `${BASE_URL}/servicedetails`;
+const URL_servicedetails = `${BASE_URL}/serviceDetails`;
+import { Checkbox } from 'react-native-paper';
 
 let idItem = '';
+let idItemD = '';
 let itemDetai = '';
 
 const ServiceManagementScreen = ({ navigation }) => {
@@ -12,20 +14,26 @@ const ServiceManagementScreen = ({ navigation }) => {
   const [detailService, setDetailService] = useState([]);
   const [modalVisibleUpdate, setModalVisibleUpdate] = useState(false);
   const [modalVisibleAdd, setModalVisibleAdd] = useState(false);
-  const [updateData, setUpdateData] = useState({
+  const [modalVisibleAddD, setModalVisibleAddD] = useState(false);
+  const [checked, setChecked] = useState(false);
+
+
+  const [updateDataService, setUpdateDataService] = useState({
     serviceName: '',
-    serviceDescription: '',
+    serviceDescription: ''
+  });
+  const [updateDataServiceD, setUpdateDataServiceD] = useState({
     title: '',
     price: ''
   });
-
-  const [addData, setAddData] = useState({
+  const [addDataService, setaddDataService] = useState({
     serviceName: '',
     serviceDescription: '',
+  });
+  const [addDataServiceD, setaddDataServiceD] = useState({
     title: '',
     price: ''
   });
-
   const servicesData = useCallback(() => {
     fetch(URL_services)
       .then(response => response.json())
@@ -45,69 +53,65 @@ const ServiceManagementScreen = ({ navigation }) => {
   }, []);
 
   useEffect(() => {
-    servicesData();
     detailServiceData();
-  }, [servicesData, detailServiceData]);
+  }, [detailServiceData]);
 
-  const handleUpdate = () => {
+  useEffect(() => {
+    servicesData();
+  }, [servicesData]);
+  // const handleUpdate = () => {
+  //   const objService = {
+  //     serviceName: updateData.serviceName,
+  //     description: updateData.serviceDescription
+  //   };
+
+  //   const objDetail = {
+  //     title: updateData.title,
+  //     price: updateData.price
+  //   };
+
+  //   fetch(URL_services + `/${idItem}`, {
+  //     method: 'PUT',
+  //     headers: {
+  //       Accept: 'application/json',
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify(objService),
+  //   })
+  //     .then(response => {
+  //       if (response.ok) {
+  //         return fetch(URL_servicedetails + `/${itemDetai}`, {
+  //           method: 'PUT',
+  //           headers: {
+  //             Accept: 'application/json',
+  //             'Content-Type': 'application/json',
+  //           },
+  //           body: JSON.stringify(objDetail),
+  //         });
+  //       } else {
+  //         console.log('');
+  //       }
+  //     })
+  //     .then(response => {
+  //       if (response.ok) {
+  //         servicesData();
+  //         detailServiceData();
+  //         Alert.alert('Cập nhật thành công');
+  //       } else {
+  //         console.log('');
+  //       }
+  //     })
+  //     .catch(error => {
+  //       Alert.alert('Thất bại đưa dữ liệu vào API: ' + error.message);
+  //       console.error(error);
+  //     });
+  // };
+
+  const ServiceAdd = () => {
     const objService = {
-      serviceName: updateData.serviceName,
-      description: updateData.serviceDescription
+      serviceName: addDataService.serviceName,
+      description: addDataService.serviceDescription
     };
-
-    const objDetail = {
-      title: updateData.title,
-      price: updateData.price
-    };
-
-    fetch(URL_services + `/${idItem}`, {
-      method: 'PUT',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(objService),
-    })
-      .then(response => {
-        if (response.ok) {
-          return fetch(URL_servicedetails + `/${itemDetai}`, {
-            method: 'PUT',
-            headers: {
-              Accept: 'application/json',
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(objDetail),
-          });
-        } else {
-          console.log('');
-        }
-      })
-      .then(response => {
-        if (response.ok) {
-          servicesData();
-          detailServiceData();
-          Alert.alert('Cập nhật thành công');
-        } else {
-          console.log('');
-        }
-      })
-      .catch(error => {
-        Alert.alert('Thất bại đưa dữ liệu vào API: ' + error.message);
-        console.error(error);
-      });
-  };
-
-  const ServiceDetailAdd = () => {
-    const objService = {
-      serviceName: addData.serviceName,
-      description: addData.serviceDescription
-    };
-
-    const objDetail = {
-      title: addData.title,
-      price: addData.price
-    };
-
     fetch(URL_services, {
       method: 'POST',
       headers: {
@@ -118,49 +122,50 @@ const ServiceManagementScreen = ({ navigation }) => {
     })
       .then(response => {
         if (response.ok) {
-          return response.json();
-        } else {
-          console.log('');
-        }
-      })
-      .then(json => {
-        const objD = {
-          idService: json._id,
-          title: objDetail.title,
-          price: objDetail.price
-        };
-
-        return fetch(URL_servicedetails, {
-          method: 'POST',
-          headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(objD),
-        });
-      })
-      .then(response => {
-        if (response.ok) {
           servicesData();
-          detailServiceData();
-          Alert.alert('Thêm thành công');
+          console.log("data" + response.json())
         } else {
           console.log('');
         }
       })
-      .catch(error => {
-        Alert.alert('Thất bại đưa dữ liệu vào API: ' + error.message);
-        console.error(error);
-      });
   };
 
+  // thêm chi tiết
+  const serverAddDetail = useCallback(() => {
+    console.log(idItemD + 'idItemD');
+    const newDetailAdd = {
+      idService: idItemD,
+      title: addDataServiceD.title,
+      price: addDataServiceD.price,
+      status: 'Đang hoạt động'
+    };
+    fetch(URL_servicedetails, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newDetailAdd)
+    }).then(response => {
+      if (response.ok) {
+        detailServiceData();
+        Alert.alert('Thêm chi tiết thành công');
+      } else {
+        Alert.alert('Thêm chi tiết thất bại');
+      }
+    });
+  });
   return (
     <SafeAreaView style={{ margin: 30 }}>
+      <Checkbox
+        status={checked ? 'checked' : 'unchecked'}
+        onPress={() => setChecked(!checked)}
+      />
       <TouchableOpacity
         onPress={() => {
           setModalVisibleAdd(!modalVisibleAdd);
         }}>
-        <Image source={require('../img/sign.png')}/>
+        <Image source={require('../img/sign.png')} />
       </TouchableOpacity>
       <Text>Services</Text>
       <FlatList
@@ -169,37 +174,33 @@ const ServiceManagementScreen = ({ navigation }) => {
         renderItem={({ item }) => (
           <View style={{ flex: 1, flexDirection: 'column', margin: 10 }}>
             <View style={styles.roww}>
-              <Image style={[styles.star, {marginRight: 40}] } source={require('../img/star.png')} />
-              <Text style={[styles.star,styles.em]}>{item.serviceName}</Text>
+              <Image style={[styles.star, { marginRight: 40 }]} source={require('../img/star.png')} />
+              <Text style={[styles.star, styles.em]}>{item.serviceName}</Text>
             </View>
             <View style={{ marginTop: 10 }}>
               <View style={{ backgroundColor: 'orange', padding: 8 }}>
                 <Text style={{ textAlign: 'center', fontSize: 15, fontWeight: 'bold', color: 'black' }}>{item.serviceName}</Text>
               </View>
-              {detailService
-                .filter((x) => x.idService._id === item._id)
+              {/* Thêm chi tiết */}
+              <TouchableOpacity
+                onPress={() => {
+                  idItemD = item._id;
+                  setModalVisibleAddD(!modalVisibleAddD);
+                }}
+              >
+                <Image source={require('../img/update.png')} />
+              </TouchableOpacity>
+              {detailService && detailService.length > 0 && detailService
+                .filter((x) => x.idService && x.idService._id === item._id)
                 .map((detailItem) => (
                   <View key={detailItem._id} style={[styles.roww, { borderEndColor: 'gray', borderWidth: 1, borderTopColor: 'white' }]}>
+                    <TouchableOpacity>
                     <Text style={[{ flex: 1, fontSize: 15, fontWeight: 'bold', color: 'black' }, styles.star]}>{detailItem.title}</Text>
-                    <TouchableOpacity
-                      style={{ flex: 1 }}
-                      onPress={() => {
-                        idItem = item._id;
-                        itemDetai = detailItem._id;
-                        setUpdateData(prevData => ({
-                          ...prevData,
-                          serviceName: item.serviceName,
-                          serviceDescription: item.description,
-                          title: detailItem.title,
-                          price: detailItem.price,
-                          statusU: detailItem.status
-                        }));
-                        setModalVisibleUpdate(!modalVisibleUpdate);
-                      }}>
-                      <Image source={require('../img/update.png')} />
+                      <Text style={[{ flex: 1, fontSize: 15, fontWeight: 'bold', color: 'black' }, styles.star]}>{detailItem.price}</Text>         
                     </TouchableOpacity>
                   </View>
                 ))}
+ {/* onPress={() => navigation.navigate('DetailServiceScreen', { idItemDetail: detailItem._id, idItemm: item._id })} */}
             </View>
           </View>
         )}
@@ -212,35 +213,27 @@ const ServiceManagementScreen = ({ navigation }) => {
         visible={modalVisibleAdd}>
         <View style={styles.centeredViewU}>
           <View style={styles.modalViewU}>
-            <Text style={[styles.modalTextU, , styles.modalCenter]}>Thêm</Text>
+            <Text style={[styles.modalTextU, styles.modalCenter]}>Thêm</Text>
             <TextInput
               placeholder='Nhập serviceName'
-              onChangeText={(text) => setAddData(prevData => ({ ...prevData, serviceName: text }))} />
+              onChangeText={(text) => setaddDataService(prevData => ({ ...prevData, serviceName: text }))} />
             <TextInput
-               placeholder='Nhập serviceDescription'
-              onChangeText={(text) => setAddData(prevData => ({ ...prevData, serviceDescription: text }))} />
-            <TextInput
-              placeholder='Nhập title'
-              onChangeText={(text) => setAddData(prevData => ({ ...prevData, title: text }))} />
-            <TextInput
-             placeholder='Nhập price'
-              onChangeText={(text) => setAddData(prevData => ({ ...prevData, price: text }))} />
-            {/* <View style={styles.roww}> */}
-              <Pressable
-                style={[ styles.buttonU, styles.buttonCloseU]} onPress={() => {
-                  ServiceDetailAdd();
-                  setModalVisibleAdd(false);
-                }}>
-                <Text  style={{ textAlign: 'center' }}>Thêm</Text>
-              </Pressable>
-              <Pressable
-                style={[  styles.buttonU, styles.buttonCloseU]}
-                onPress={() =>
-                  setModalVisibleAdd(false)
-                }>
-                <Text style={{ textAlign: 'center' }}>Hủy</Text>
-              </Pressable>
-            {/* </View> */}
+              placeholder='Nhập serviceDescription'
+              onChangeText={(text) => setaddDataService(prevData => ({ ...prevData, serviceDescription: text }))} />
+            <Pressable
+              style={[styles.buttonU, styles.buttonCloseU]} onPress={() => {
+                ServiceAdd();
+                setModalVisibleAdd(false);
+              }}>
+              <Text style={{ textAlign: 'center' }}>Thêm</Text>
+            </Pressable>
+            <Pressable
+              style={[styles.buttonU, styles.buttonCloseU]}
+              onPress={() =>
+                setModalVisibleAdd(false)
+              }>
+              <Text style={{ textAlign: 'center' }}>Hủy</Text>
+            </Pressable>
           </View>
         </View>
       </Modal>
@@ -248,11 +241,10 @@ const ServiceManagementScreen = ({ navigation }) => {
       <Modal
         animationType="slide"
         transparent={true}
-        visible={modalVisibleUpdate}
-      >
+        visible={modalVisibleUpdate}>
         <View style={styles.centeredViewU}>
           <View style={styles.modalViewU}>
-            <Text style={[styles.modalTextU, styles.modalCenter  ]}>Cập nhật</Text>
+            <Text style={[styles.modalTextU, styles.modalCenter]}>Cập nhật</Text>
             <TextInput
               placeholder='Nhập serviceName'
               onChangeText={(text) => setUpdateData(prevData => ({ ...prevData, serviceName: text }))} />
@@ -268,7 +260,6 @@ const ServiceManagementScreen = ({ navigation }) => {
             <Pressable
               style={[styles.buttonU, styles.buttonCloseU]}
               onPress={() => {
-                handleUpdate();
                 setModalVisibleUpdate(false);
               }}>
               <Text>Cập nhật</Text>
@@ -276,6 +267,37 @@ const ServiceManagementScreen = ({ navigation }) => {
             <Pressable
               style={[styles.buttonU, styles.buttonCloseU]}
               onPress={() => setModalVisibleUpdate(false)}>
+              <Text style={{ textAlign: 'center' }}>Hủy</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+      {/* thêm chi tiết */}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisibleAddD}>
+        <View style={styles.centeredViewU}>
+          <View style={styles.modalViewU}>
+            <Text style={[styles.modalTextU, styles.modalCenter]}>Thêm chi tiết</Text>
+            <TextInput
+              placeholder='Nhập title'
+              onChangeText={(text) => setaddDataServiceD(prevData => ({ ...prevData, title: text }))} />
+            <TextInput
+              placeholder='Nhập price'
+              onChangeText={(text) => setaddDataServiceD(prevData => ({ ...prevData, price: text }))} />
+            <Pressable
+              style={[styles.buttonU, styles.buttonCloseU]} onPress={() => {
+                serverAddDetail();
+                setModalVisibleAddD(false);
+              }}>
+              <Text style={{ textAlign: 'center' }}>Thêm chi tiết</Text>
+            </Pressable>
+            <Pressable
+              style={[styles.buttonU, styles.buttonCloseU]}
+              onPress={() =>
+                setModalVisibleAddD(false)
+              }>
               <Text style={{ textAlign: 'center' }}>Hủy</Text>
             </Pressable>
           </View>
@@ -315,22 +337,24 @@ const styles = StyleSheet.create({
     marginBottom: 15,
     color: 'black'
   },
-  modalCenter:{
+  modalCenter: {
     fontSize: 20,
-     fontWeight: 'bold',
-      alignSelf: 'center'
+    fontWeight: 'bold',
+    alignSelf: 'center'
   },
   roww: {
     flex: 1,
     flexDirection: 'row'
   },
-  star:{
+  star: {
     alignItems: 'center',
-     alignSelf: 'center'
+    alignSelf: 'center'
   },
-  em:{
+  em: {
     fontSize: 20,
-     fontWeight: 'bold',
-      color: 'black' 
+    fontWeight: 'bold',
+    color: 'black'
   }
 });
+
+
