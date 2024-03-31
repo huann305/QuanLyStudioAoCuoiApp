0db63e530155d336a452c6b25f05d94f5c8968a1
anhktn <nanacute.tk.1@gmail.com>
2024-03-31 17:54:45 +0700

    anhktn_employee_img

--- App.js
+++ App.js
@@ -142,7 +142,7 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="WelcomeScreen"
+        initialRouteName="Login"
         screenOptions={{headerShown: false}}>
         <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} />
         <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
@@ -157,14 +157,14 @@ const App = () => {
           name="EmployeeManagementScreen"
           component={EmployeeManagementScreen}
         />
-         <Stack.Screen
+        <Stack.Screen
           name="DetailServiceScreen"
           component={DetailServiceScreen}
         />
-         <Stack.Screen
+        <Stack.Screen
           name="ServiceManagementScreen"
           component={ServiceManagementScreen}
-          />
+        />
         <Stack.Screen
           name="DetailEmployeeScreen"
           component={DetailEmployeeScreen}
